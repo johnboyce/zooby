df2d16947a0634d6a5b634b41fea2a7d09576c97
johnboyce <johnboyce@comcast.net>
2025-05-23 02:27:25 -0400

    Setup AWS_S3_FORCE_PATH_STYLE

--- infra/main.tf
+++ infra/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
   secret_key                  = "mock_secret_key"
   skip_credentials_validation = true
   skip_metadata_api_check     = true
-  s3_force_path_style         = true
+
   endpoints {
     dynamodb = "http://localhost:4566"
     sqs      = "http://localhost:4566"
