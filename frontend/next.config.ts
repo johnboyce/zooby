5518db018d51615f18f9fa392f783fedfd18beb8
johnboyce <johnboyce@comcast.net>
2025-06-01 11:20:50 -0400

    fix: gh-pages set asset path
    Why: images can not yet be found

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'out',
   basePath: process.env.NODE_ENV === 'production' ? '/zooby' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/zooby' : '',
   images: {
     unoptimized: true,
   },
